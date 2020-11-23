0c1abb580ef062652e674b1283e167188fbdefe6
elephantheatsink <elephantheatsink@gmail.com>
2020-11-23 12:15:08 -0800

    added routes

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import React from 'react';
 
 // import { Route, BrowserRouter as Router, Switch} from 'react-router-dom'
 import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import Homepage from './components/Homepage'
+import Homepage from './components/Homepage';
+import Violin from './components/Violin';
+import Starcraft from './components/Starcraft';
+import Fitness from './components/Fitness';
+import Coding from './components/Coding';
 
 import './App.css';
 
@@ -22,6 +26,10 @@ function App() {
             <Switch>
 
               <Route path='/' exact component={Homepage}/>
+              <Route path='/violin' exact component={Violin}/>
+              <Route path='/starcraft' exact component={Starcraft}/>
+              <Route path='/fitness' exact component={Fitness}/>
+              <Route path='/coding' exact component={Coding}/>
            
 
             </Switch>
---
+++ src/components/Fitness.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import {Container} from 'react-bootstrap'
+
+
+class Fitness extends Component {
+
+     
+      render() {
+          return (
+
+                  <React.Fragment>
+                  <div>
+                          <Container style={{
+                              backgroundColor: '#717C7150',
+                              borderRadius: '5px',
+                              width: '530px',
+                               padding: '30px',
+                               marginTop: '20px'
+                              }}>
+                              <a href='/'>
+                                  Back to Home
+                              <br></br>
+                              </a>
+                              <br></br>
+                              This page is about fitness
+                              <br></br>
+                              <br></br>
+                              
+                          </Container>
+                  </div>   
+              </React.Fragment>
+  
+          );
+      }
+}
+export default Fitness
